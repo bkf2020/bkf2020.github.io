1ba75088b7caca856b2cef730cb940eacf7e7145
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-09-03 22:22:51 -0700

    initial version of amcgen

---
+++ amcgen/README.md
@@ -0,0 +1,2 @@
+# amcgen
+amcgen provides lists of aime, amc 8, amc 10, and amc 12 problems for you to practice. these lists are updated daily.
---
+++ amcgen/gen.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+DATE=$(date "+%Y/%m/%d")
+AIMEDIR="aime/"$DATE
+AMC8DIR="amc8/"$DATE
+AMC10DIR="amc10/"$DATE
+AMC12DIR="amc12/"$DATE
+
+mkdir -p $AIMEDIR
+mkdir -p $AMC8DIR
+mkdir -p $AMC10DIR
+mkdir -p $AMC12DIR
+
+for TEST in aime amc8 amc10 amc12
+do
+	for LEVEL in easy medium hard veryhard
+	do
+		CHOICES_PATH=choices/$TEST/$LEVEL
+		PROBLEMS_PATH=choices/$TEST/$LEVEL"_names"
+		TEMPLATE_PATH=templates/$TEST/$LEVEL".html"
+		PROBLEMSET_FILE=$TEST/$DATE/$LEVEL".html"
+		touch $PROBLEMSET_FILE
+
+		LINE_NUMBERS=$(wc -l < $CHOICES_PATH)
+		shuf -i 1-$LINE_NUMBERS -n 5 > out
+		URL1=$(sed -n $(sed -n '1p' out)p $CHOICES_PATH)
+		URL2=$(sed -n $(sed -n '2p' out)p $CHOICES_PATH)
+		URL3=$(sed -n $(sed -n '3p' out)p $CHOICES_PATH)
+		URL4=$(sed -n $(sed -n '4p' out)p $CHOICES_PATH)
+		URL5=$(sed -n $(sed -n '5p' out)p $CHOICES_PATH)
+		PRO1=$(sed -n $(sed -n '1p' out)p $PROBLEMS_PATH)
+		PRO2=$(sed -n $(sed -n '2p' out)p $PROBLEMS_PATH)
+		PRO3=$(sed -n $(sed -n '3p' out)p $PROBLEMS_PATH)
+		PRO4=$(sed -n $(sed -n '4p' out)p $PROBLEMS_PATH)
+		PRO5=$(sed -n $(sed -n '5p' out)p $PROBLEMS_PATH)
+		export URL1; export URL2; export URL3; export URL4; export URL5; \
+		export PRO1; export PRO2; export PRO3; export PRO4; export PRO5; \
+		envsubst < $TEMPLATE_PATH > $PROBLEMSET_FILE; \
+		export AIMEDIR; export AMC8DIR; export AMC10DIR; export AMC12DIR; \
+		envsubst < templates/index_template.html > index.html; \
+	done
+done
+
+# delete out file
+rm out
