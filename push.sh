8bfd05b9490b8cbbd41c96415b34c96843e8dfcd
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-12-25 21:32:56 -0800

    update push script to alert if I didn't update visual.html

--- push.sh
+++ push.sh
@@ -3,3 +3,13 @@
 git push
 git push --prune https://gitlab.com/bkf2020/bkf2020.gitlab.io.git
 git push --prune https://codeberg.org/bkf2020/pages.git
+
+math=$(grep -F '<th class="so">Solved</th>' mathlog.html | wc -l)
+cs=$(grep -F '<th class="so">Solved</th>' cslog.html | wc -l)
+total=$(echo "${math} + ${cs}" | bc)
+
+grep -F 'var problemsSolved' visual.html > curr
+echo "var problemsSolved = ${total};" > new
+diff -w curr new || echo -e "\033[0;31mYou need to update visual.html! \033[0m"
+
+rm curr new
