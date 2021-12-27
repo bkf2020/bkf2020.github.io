a5c7c5a069fcf99ee9ed879e38425ea7c3cf9131
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2021-12-26 19:31:17 -0800

    Script to get count of total problems solved

---
+++ getcount.sh
@@ -0,0 +1,9 @@
+math=$(grep -F '<th class="so">Solved</th>' mathlog.html | wc -l)
+cs=$(grep -F '<th class="so">Solved</th>' cslog.html | wc -l)
+total=$(echo "${math} + ${cs}" | bc)
+
+grep -F 'var problemsSolved' visual.html > curr
+echo "var problemsSolved = ${total};" > new
+diff -w curr new || echo -e "\033[0;31mYou need to update visual.html with ${total} problems solved! \033[0m"
+
+rm curr new
